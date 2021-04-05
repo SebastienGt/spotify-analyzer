9d6afe00680a12ab6189873f0c6a136e1a397e87
Sébastien <sebastien.friedberg@gmail.com>
2021-04-05 12:49:30 +0200

    Adding package

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -36,7 +36,7 @@ const UserProfile = () => {
                 </>
             ) : (
                     <h4>Reconnectez-vous</h4>
-                    )
+                )
             }
 
     </div>
