5afe1b2125545a81d3aef51b989d640f41d0c656
Sébastien <sebastien.friedberg@gmail.com>
2021-04-16 16:14:49 +0200

    Adding redirect link

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -107,7 +107,8 @@ app.get('/callback', function(req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect('http://localhost:3000/#' +
+        //res.redirect('http://localhost:3000/#' +
+        res.redirect('https://mighty-shore-31251.herokuapp.com/#' +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
